18e9d985f7dcc49b7acc5116515da6c50cc62fce
Swapnil <resoniteswapnil@gmail.com>
2026-02-12 12:13:44 +0530

    DAY 1: basics, input , conditions , loops

---
+++ Day1basics/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Enter a number: ");
+int number = Convert.ToInt32
+(Console.ReadLine());
+
+if (number % 2 == 0)
+{
+    Console.WriteLine("Even number");
+}
+else
+{
+    Console.WriteLine("Odd number");
+}
+Console.WriteLine ("Thank you for using the program.");
