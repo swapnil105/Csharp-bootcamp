8ff05b15c1c90dd26c2d27659f0a0b5e269a1876
Swapnil <resoniteswapnil@gmail.com>
2026-02-13 03:49:50 +0530

    Day 2 methods, parameters,return values, intro to data types

--- Day1basics/Program.cs
+++ Day1basics/Program.cs
@@ -1,13 +1,33 @@
-﻿Console.Write("Enter a number: ");
-int number = Convert.ToInt32
-(Console.ReadLine());
+﻿namespace _04_02_XX_Exercicio_01
+{
+    class Program
+    {
+        static void Main(string[] args) //main is also a method but it is the entry point of the program
+        {
+            Console.Write("Enter name: ");
+            string characterName = Console.ReadLine();           //yha pe hmlog characterName ko input kr skte h
 
-if (number % 2 == 0)
-{
-    Console.WriteLine("Even number");
+            Console.Write("Enter age: ");
+            int characterAge = Convert.ToInt32(Console.ReadLine());    //yha pe hmlog characterAge ko input kr skte h
+
+         
+            Tellstory(characterName, characterAge);
+        }
+
+        static void Tellstory(string characterName, int characterAge)
+        {      //this part is method , evrything inside this {} is method body            
+            Console.WriteLine("There once was a man named " + characterName);
+            Console.WriteLine("He was " + characterAge +" old");
+
+            characterName = "Trump";   //yha pe hmlog kbhi bhi characterName ko change kr skte h 
+            Console.WriteLine("He really liked the name " + characterName);
+
+            characterAge = 62;   // yha pe hmlog kbhi bhi characterAge ko change kr skte h
+            Console.WriteLine("But didn't like being " + characterAge );
+            Console.WriteLine("He wished he was kid again but he was too old for that");
+           
+
+            
+        }
+    }
 }
-else
-{
-    Console.WriteLine("Odd number");
-}
-Console.WriteLine ("Thank you for using the program.");
---
+++ Day2Practice/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.Design;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices.Marshalling;
+
+namespace _Day2Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            //string name = "Boss";
+            //Console.WriteLine($"i am the boss , {name} is my name");
+         
+
+        int a = 9;
+        double b = 3.14;
+        Console.WriteLine(a + b);
+    }
+    }
+}
