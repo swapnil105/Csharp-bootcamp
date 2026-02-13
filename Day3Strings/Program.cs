abdc1c764173d82d9a16a69b5ba756a6fc97e60d
Swapnil <resoniteswapnil@gmail.com>
2026-02-14 04:18:48 +0530

    Day 3: email validation, string checks, reusable method

---
+++ Day3Strings/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _Day3Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {  
+             Console.Write("Enter text: ");
+             string phrase = Console.ReadLine();
+
+
+             bool result = IsValidEmail(input);
+             Console.WriteLine(result);
+
+                {
+                    return email.Contains("@") && email.Contains(".com");
+                }
+        
+             if (phrase .Contains("@") && phrase .Contains(".com"))
+                {
+                    Console.WriteLine("valid email");
+                }
+                else
+                {
+                    Console.WriteLine("invalid email");
+                }
+        }
+     }
+}
